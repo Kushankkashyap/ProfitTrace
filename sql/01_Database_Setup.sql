e57494e6c50de58cef4b6e59bcd221ba1c0541db
Kushank Kashyap <Kushkashyap87@gmail.com>
2026-09-15 00:05:42 +0530

    Rebuild ProfitTrace database setup for V2 source model

--- sql/01_Database_Setup.sql
+++ sql/01_Database_Setup.sql
@@ -1,22 +1,16 @@
 /*
     ProfitTrace | Database Setup
     SQL Server / T-SQL
-
-    Creates the project database and schemas.
-    Raw Excel data is imported into the stg schema in the next step.
 */
-
-IF DB_ID(N'ProfitTrace') IS NULL
-BEGIN
+IF DB_ID('ProfitTrace') IS NULL
     CREATE DATABASE ProfitTrace;
-END;
 GO
 
 USE ProfitTrace;
 GO
 
-IF SCHEMA_ID(N'stg') IS NULL EXEC(N'CREATE SCHEMA stg');
-IF SCHEMA_ID(N'analytics') IS NULL EXEC(N'CREATE SCHEMA analytics');
+IF SCHEMA_ID('stg') IS NULL EXEC('CREATE SCHEMA stg');
+IF SCHEMA_ID('analytics') IS NULL EXEC('CREATE SCHEMA analytics');
 GO
 
 DROP VIEW IF EXISTS analytics.vw_OrderProfitability;
@@ -24,8 +18,8 @@ DROP VIEW IF EXISTS analytics.vw_ReturnsOperations;
 DROP VIEW IF EXISTS analytics.vw_CustomerProfitability;
 GO
 
-DROP TABLE IF EXISTS stg.Shipping;
 DROP TABLE IF EXISTS stg.Returns;
+DROP TABLE IF EXISTS stg.Shipping;
 DROP TABLE IF EXISTS stg.Orders;
 DROP TABLE IF EXISTS stg.Products;
 DROP TABLE IF EXISTS stg.Customers;
